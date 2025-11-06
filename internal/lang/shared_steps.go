66f5763bb3f9fd49e472e93c2fc0a106ce577962
guineveresaenger <guinevere@pulumi.com>
2025-11-06 11:43:14 -0800

    Conditionally add the --overlay flag to Pulumi SDK command if a directory called overlays exists

--- internal/lang/shared_steps.go
+++ internal/lang/shared_steps.go
@@ -1,6 +1,9 @@
 package lang
 
 import (
+	"errors"
+	"io/fs"
+	"os"
 	"path/filepath"
 	"strings"
 )
@@ -12,13 +15,20 @@ const (
 
 // BaseGenerateSdkCommand creates shell commands for generating an SDK and copying README and LICENSE files
 func BaseGenerateSdkCommand(schemaPath, outputPath, language, version, providerPath string) []string {
-
 	// Generate SDK command
 	sdkCmd := pulumiPackageGenSdkCmd
 	sdkCmd = strings.ReplaceAll(sdkCmd, "{Path}", schemaPath)
 	sdkCmd = strings.ReplaceAll(sdkCmd, "{Lang}", language)
 	sdkCmd = strings.ReplaceAll(sdkCmd, "{OutputPath}", outputPath)
 	sdkCmd = strings.ReplaceAll(sdkCmd, "{Version}", version)
+
+	// Providers are expected to keep their overlays in an `./overlays` directory.
+	// Add --overlays flag if overlay directory exists
+	overlayDirLocation := filepath.Join(providerPath, "overlays")
+	info, err := os.Stat(overlayDirLocation)
+	if !errors.Is(err, fs.ErrNotExist) && info.IsDir() {
+		sdkCmd += " --overlays \"" + overlayDirLocation + "\""
+	}
 
 	// Generate file copy commands
 	readmeCmd := generateCopyCommand(providerPath, "README.md", outputPath, language, "README.md")
